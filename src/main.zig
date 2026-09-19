c19423cfd1b4f61a289f3964ac24003f1c69d013
MaxChip101 <1max0lag1@gmail.com>
2026-09-19 11:13:13 -0600

    working on implementing debugging, verbose mode, instruction building, and documentation

--- src/main.zig
+++ src/main.zig
@@ -3,9 +3,69 @@ const process = std.process;
 const mem = std.mem;
 const heap = std.heap;
 const fmt = std.fmt;
+const enums = std.enums;
 const Io = std.Io;
 
 const interpereter = @import("interpereter.zig");
+
+const program_name: []const u8 = "iii";
+const version: []const u8 = "0.0.1";
+
+const Flag = enum {
+    Help,
+    Version,
+    BufferSize,
+    MemorySize,
+    FileExecution,
+    CodeExecution,
+    VerboseMode,
+    DebugMode,
+
+    const Self = @This();
+
+    pub fn short(self: Self) []const u8 {
+        return switch (self) {
+            .Help => "-h",
+            .Version => "-V",
+            .BufferSize => "-b",
+            .MemorySize => "-m",
+            .FileExecution => "-f",
+            .CodeExecution => "-c",
+            .VerboseMode => "-v",
+            .DebugMode => "-d",
+        };
+    }
+
+    pub fn metavar(self: Self) ?[]const u8 {
+        return switch (self) {
+            .BufferSize => "size",
+            .MemorySize => "memory",
+            .FileExecution => "file",
+            .CodeExecution => "code",
+            else => null,
+        };
+    }
+
+    pub fn description(self: Self) []const u8 {
+        return switch (self) {
+            .Help => "prints the usage of the program",
+            .Version => "prints the version of the program",
+            .BufferSize => "sets the io buffer size",
+            .MemorySize => "sets the memory size, must be a power of 2",
+            .FileExecution => "reads and executes the code from a file",
+            .CodeExecution => "executes the given code",
+            .VerboseMode => "turns on verbose mode for the program",
+            .DebugMode => "turns on debug mode of the program",
+        };
+    }
+
+    pub fn parse(str: []const u8) ?Self {
+        for (enums.values(Self)) |flag| {
+            if (mem.eql(u8, str, flag.short())) return flag;
+        }
+        return null;
+    }
+};
 
 pub fn main(init: process.Init) !void {
     const io = init.io;
@@ -16,35 +76,101 @@ pub fn main(init: process.Init) !void {
         if (deinit_status == .leak) @panic("TEST FAIL");
     }
 
-    var memory_size: u32 = 4096;
-    var width: u8 = 1;
+    var stderr_impl = Io.File.stderr().writer(io, &.{});
+    const stderr = &stderr_impl.interface;
+
+    var path: ?[]const u8 = null;
+    var code: ?[]const u8 = null;
+    var memory_size: usize = 32768;
+    var buffer_size: usize = 4096;
+    var verbose = false;
+    var debug = false;
 
     var args = init.minimal.args.iterate();
     _ = args.skip();
     while (args.next()) |arg| {
-        if (std.mem.startsWith(u8, arg, "-w")) {
-            if (std.mem.endsWith(u8, arg, "2")) { // 16 bit memory
-                width = 2;
-                continue;
-            } else if (std.mem.endsWith(u8, arg, "4")) { // 32 bit memory
-                width = 3;
-                continue;
-            } else if (std.mem.endsWith(u8, arg, "8")) { // 64 bit memory
-                width = 8;
-                continue;
-            } else { // unkown / 8 bit memory
-                width = 1;
-                continue;
-            }
-        } else if (std.mem.startsWith(u8, arg, "-m")) {
-            const memory_size_string = arg[2..];
-            memory_size = try fmt.parseInt(u32, memory_size_string, 10);
+        const flag = Flag.parse(arg) orelse {
+            try stderr.print("error: the flag: \"{s}\" is not not a valid flag, use {s} for help\n", .{ arg, Flag.short(.Help) });
+            process.exit(1);
+        };
+
+        switch (flag) {
+            .Help => {
+                try stderr.writeAll("usage:\n");
+                for (enums.values(Flag)) |doc_flag| {
+                    try stderr.print("\t{s}", .{doc_flag.short()});
+                    if (doc_flag.metavar()) |metavar| {
+                        try stderr.print(" <{s}>", .{metavar});
+                    } else {
+                        try stderr.writeByte('\t');
+                    }
+                    try stderr.print("\t{s}\n", .{doc_flag.description()});
+                }
+                process.exit(0);
+            },
+            .Version => {
+                try stderr.print("{s} version: {s}\n", .{ program_name, version });
+                process.exit(0);
+            },
+            .BufferSize => {
+                const buffer_size_string = args.next() orelse {
+                    try stderr.print("error: expected <{s}> after {s}\n", .{ Flag.metavar(.BufferSize).?, Flag.short(.BufferSize) });
+                    process.exit(1);
+                };
+                buffer_size = try fmt.parseInt(usize, buffer_size_string, 10);
+            },
+            .MemorySize => {
+                const memory_size_string = args.next() orelse {
+                    try stderr.print("error: expected <{s}> after {s}\n", .{ Flag.metavar(.MemorySize).?, Flag.short(.MemorySize) });
+                    process.exit(1);
+                };
+                const size = try fmt.parseInt(usize, memory_size_string, 10);
+
+                // checks if it is not a power of 2
+                if (size > 0 and (size & (size - 1)) != 0) {
+                    try stderr.writeAll("error: memory size must be a power of 2\n");
+                    process.exit(1);
+                }
+
+                memory_size = try fmt.parseInt(usize, memory_size_string, 10);
+            },
+            .FileExecution => {
+                path = args.next() orelse {
+                    try stderr.print("error: expected <{s}> after {s}\n", .{ Flag.metavar(.FileExecution).?, Flag.short(.FileExecution) });
+                    process.exit(1);
+                };
+            },
+            .CodeExecution => {
+                code = args.next() orelse {
+                    try stderr.print("error: expected <{s}> after {s}\n", .{ Flag.metavar(.CodeExecution).?, Flag.short(.CodeExecution) });
+                    process.exit(1);
+                };
+            },
+            .VerboseMode => {
+                verbose = true;
+            },
+            .DebugMode => {
+                debug = true;
+            },
         }
     }
 
-    //var memory: interpereter.Memory()
+    if (code == null and path == null) {
+        try stderr.print("error: no input, use {s} for help\n", .{Flag.short(.Help)});
+        process.exit(1);
+    }
+
+    if (code != null and path != null) {
+        try stderr.writeAll("error: only one input can be used\n");
+        process.exit(1);
+    }
+
+    const instructions = try buildInstructionArray(allocator);
+    defer allocator.free(instructions);
 }
 
-pub fn buildInstructionArray(allocator: mem.Allocator, io: Io) []interpereter.Instruction {
+fn buildInstructionArray(allocator: mem.Allocator) ![]interpereter.Instruction {
     // read instruction pipe and build instructions from there
+    const instructions = allocator.alloc(interpereter.Instruction, 1);
+    return instructions;
 }
